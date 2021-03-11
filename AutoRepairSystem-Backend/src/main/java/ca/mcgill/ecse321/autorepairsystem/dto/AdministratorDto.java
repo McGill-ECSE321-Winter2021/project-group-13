00836b80915940e8708fe55115ae1bbcf9d46aca
Ezra <ezra.gomolin@mail.mcgill.ca>
2021-03-11 15:19:04 -0500

    Administrator dto

--- AutoRepairSystem-Backend/src/main/java/ca/mcgill/ecse321/autorepairsystem/dto/AdministratorDto.java
+++ AutoRepairSystem-Backend/src/main/java/ca/mcgill/ecse321/autorepairsystem/dto/AdministratorDto.java
@@ -1,5 +1,17 @@
 package ca.mcgill.ecse321.autorepairsystem.dto;
 
-public class AdministratorDto {
+public class AdministratorDto extends UserDto {
 
+	public AdministratorDto() {
+		
+	}
+	
+	public AdministratorDto(String email) {
+		this(null,null,null,email);
+	}
+
+	public AdministratorDto(String username, String password, String name, String email){
+		super(username, password, name, email);
+		
+	}
 }
