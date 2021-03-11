e9b58f125ed51b65d7248f6a27f3f2660aaf748d
Ezra <ezra.gomolin@mail.mcgill.ca>
2021-03-11 16:28:23 -0500

    Appointment, AppointmentManager DTO's

--- AutoRepairSystem-Backend/src/main/java/ca/mcgill/ecse321/autorepairsystem/dto/AppointmentDto.java
+++ AutoRepairSystem-Backend/src/main/java/ca/mcgill/ecse321/autorepairsystem/dto/AppointmentDto.java
@@ -1,5 +1,85 @@
 package ca.mcgill.ecse321.autorepairsystem.dto;
 
-public class AppointmentDto {
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Set;
 
+public class AppointmentDto {
+	private Set <ServiceDto> services;
+	private TechnicianDto technician;
+	private CustomerDto customer;
+	private Integer id;
+	private Time starttime;
+	private Time endtime;
+	private Date date;
+	
+	public AppointmentDto() {}
+	
+	public AppointmentDto(Set <ServiceDto> services,TechnicianDto technician,CustomerDto customer,Integer id, Time starttime, Time endtime, Date date) {
+		this.services = services;
+		this.technician = technician;
+		this.customer = customer;
+		this.id = id;
+		this.starttime = starttime;
+		this.endtime = endtime;
+		this.date = date;
+			
+	}
+	
+	public void setServices(Set <ServiceDto> services) {
+		this.services = services;
+	}
+	
+	public Set <ServiceDto> getServices(){
+		return services;
+	}
+	
+	public void setTechnician(TechnicianDto technician) {
+		this.technician = technician;
+	}
+	
+	public TechnicianDto getTechnician() {
+		return technician;
+	}
+	
+	public void setCustomer(CustomerDto customer) {
+		this.customer = customer;
+	}
+	
+	public CustomerDto getCustomer() {
+		return customer;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setStartTime(Time starttime) {
+		this.starttime = starttime;
+	}
+	
+	public Time getStartTime() {
+		return starttime;
+	}
+	
+	public void setEndTime(Time endtime) {
+		this.endtime = endtime;
+	}
+	
+	public Time getEndTime() {
+		return endtime;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
 }
--- AutoRepairSystem-Backend/src/main/java/ca/mcgill/ecse321/autorepairsystem/dto/AppointmentManagerDto.java
+++ AutoRepairSystem-Backend/src/main/java/ca/mcgill/ecse321/autorepairsystem/dto/AppointmentManagerDto.java
@@ -1,5 +1,60 @@
 package ca.mcgill.ecse321.autorepairsystem.dto;
 
+import java.util.Collections;
+import java.util.Set;
+
 public class AppointmentManagerDto {
 
+	private Set <UserDto> users;
+	private Set <ServiceDto> services;
+	private Set <BusinessHourDto> businesshours;
+	private Set <AppointmentDto> appointments;
+	
+	public AppointmentManagerDto() {}
+	
+	@SuppressWarnings("unchecked")
+	public AppointmentManagerDto(Set <ServiceDto> services,Set <BusinessHourDto> businesshours) {
+		this(Collections.EMPTY_SET, services, businesshours, Collections.EMPTY_SET);
+	}
+	
+	public AppointmentManagerDto(Set <UserDto> users, Set <ServiceDto> services,Set <BusinessHourDto> businesshours, Set <AppointmentDto> appointments) {
+		this.users = users;
+		this.services = services;
+		this.businesshours = businesshours;
+		this.appointments = appointments;
+	}
+	
+	public Set <AppointmentDto> getAppointments(){
+		return appointments;
+	}
+	
+	public void setAppointments(Set <AppointmentDto> appointments) {
+		this.appointments = appointments;
+	}
+	
+	public Set <UserDto> getUsers(){
+		return users;
+	}
+	
+	public void setUsers(Set <UserDto> users) {
+		this.users = users;
+	}
+	
+	public Set <BusinessHourDto> getBusinessHours(){
+		return businesshours;
+	}
+	
+	public void setBusinessHours(Set <BusinessHourDto> businesshours) {
+		this.businesshours = businesshours;
+	}
+	
+	public Set <ServiceDto> getServices(){
+		return services;
+	}
+	
+	public void setServices(Set <ServiceDto> services) {
+		this.services = services;
+	}
+	
+	
 }
