ec82e57dd50bb55d75a82f442c75e13ff340f36a
Ezra <ezra.gomolin@mail.mcgill.ca>
2021-03-23 15:44:12 -0400

    small change

---
+++ AutoRepairSystem-frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h2> Auto Repair Online System </h2>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: #f2ece8;
+  margin-top: 60px;
+}
+</style>
