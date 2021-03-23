4609c25588c14825b0aa73434af51275593dec17
Ezra <ezra.gomolin@mail.mcgill.ca>
2021-03-23 11:42:41 -0400

    Added Some Views (all pages are same as tutorial content)

--- AutoRepaiSystem-frontend/src/router/index.js
+++ AutoRepaiSystem-frontend/src/router/index.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import SignIn from "@/components/SignIn"
+import CustomerHome from "@/components/CustomerHome"
+import TechnicianHome from "@/components/TechnicianHome"
+import AdministratorHome from "@/components/AdministratorHome"
+import AvailableAppointments from "@/components/AvailableAppointments"
+import BookAppointmentConfirmation from "@/components/BookAppointmentConfirmation"
+import ViewAllTechnicians from "@/components/ViewAllTechnicians"
+import ViewAllCustomer from "@/components/ViewAllCustomer"
+import ViewAllAppointments from "@/components/ViewAllAppointments"
+import PayInvoice from "@/components/PayInvoice"
 
 Vue.use(Router)
 
@@ -8,8 +17,57 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'SignIn',
+      component: SignIn
+    } ,
+    {
+      path: '/CustomerHome',
+      name: 'CustomerHome',
+      component: CustomerHome
+    } ,
+    {
+      path: '/TechnicianHome',
+      name: 'TechnicianHome',
+      component: TechnicianHome
+    } ,
+    {
+      path: '/AdministratorHome',
+      name: 'AdministratorHome',
+      component: AdministratorHome
+    } , 
+    {
+      path: '/AvailableAppointments',
+      name: 'AvailableAppointments',
+      component: AvailableAppointments
+    }, 
+    {
+      path: '/BookAppointmentConfirmation',
+      name: 'BookAppointmentConfirmation',
+      component: BookAppointmentConfirmation
+    }, 
+    {
+      path: '/ViewAllTechnicians',
+      name: 'ViewAllTechnicians',
+      component: ViewAllTechnicians
     }
+    , 
+    {
+      path: '/ViewAllCustomer',
+      name: 'ViewAllCustomer',
+      component: ViewAllCustomer
+    } , 
+    {
+      path: '/ViewAllAppointments',
+      name: 'ViewAllAppointments',
+      component: ViewAllAppointments
+    }
+    , 
+    {
+      path: '/PayInvoice',
+      name: 'PayInvoice',
+      component: PayInvoice
+    }
+    
+    
   ]
 })
