abcbbd0a27cd3cc75e65eb3e6afba753d6223f7e
cWetaski <charles.wetaski@mail.mcgill.ca>
2021-03-26 13:43:27 -0400

    Added current user

--- AutoRepairSystem-Backend/src/main/java/ca/mcgill/ecse321/autorepairsystem/AutoRepairSystemApplication.java
+++ AutoRepairSystem-Backend/src/main/java/ca/mcgill/ecse321/autorepairsystem/AutoRepairSystemApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ca.mcgill.ecse321.autorepairsystem.model.EndUser;
 
 @RestController
 @SpringBootApplication
 public class AutoRepairSystemApplication {
+	
+	private static EndUser currentUser;
 
 	public static void main(String[] args) {
 	  
@@ -19,5 +22,16 @@ public class AutoRepairSystemApplication {
 	public String greeting(){
 		return "Hello world! ECSE 321";
 	}
-
+	
+    public static EndUser getCurrentUser() {
+    	if (currentUser != null) {
+    		return currentUser;
+    	} else {
+    		return null;
+    	}
+    }
+    
+    public static void setCurrentUser(EndUser curUser) {
+    	currentUser = curUser;
+    }
 }
