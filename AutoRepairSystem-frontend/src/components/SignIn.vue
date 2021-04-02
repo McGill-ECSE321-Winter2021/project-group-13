9b4d63174938d8fa0d87bc4b7762d852e9de6223
hassan kanj <kanjhassan@gmail.com>
2021-04-03 00:37:07 +0300

    Signin fix

--- AutoRepairSystem-frontend/src/components/SignIn.vue
+++ AutoRepairSystem-frontend/src/components/SignIn.vue
@@ -25,7 +25,7 @@
     <div>
       <span v-if="errorMessage" style="color:red">Error: {{errorMessage}}</span>
       <br>  
-      <button id="button" class="btn-hover color" v-bind:disabled="!username || !password || !userType" @click="isValidType(userType, username, password)"> Log In </button>
+      <button id="button" class="btn-hover color" v-bind:disabled="!username || !password" @click="signIn(username, password)"> Log In </button>
       <br><br>
       <span>Create an account? <router-link to="/AccountCreation">Sign up</router-link></span>
     </div>
