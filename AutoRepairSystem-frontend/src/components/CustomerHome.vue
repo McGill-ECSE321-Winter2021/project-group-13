33a7721ad1972dcebe5eae18f069248a6520602e
edgardesnos <edgar.desnos@gmail.com>
2021-04-03 18:00:35 -0400

    CustomerHome services display correctly and fixed deleteUser bug

--- AutoRepairSystem-frontend/src/components/CustomerHome.vue
+++ AutoRepairSystem-frontend/src/components/CustomerHome.vue
@@ -18,7 +18,7 @@
   <table class="styled-table">
     <thead>
         <tr>
-            <th>Service</th>
+            <th>Services</th>
             <th>Date</th>
             <th>Start Time</th>
             <th>End Time</th>
@@ -26,7 +26,7 @@
     </thead>
       <tbody>
           <tr v-for="appointment in appointments">
-            <td>{{ appointment.services.name }}</td>
+            <td>{{ formattedServices(appointment) }}</td>
             <td>{{ appointment.date }}</td>
             <td>{{ appointment.startTime }}</td>
             <td>{{ appointment.endTime }}</td>
@@ -222,7 +222,7 @@ export default {
     },
     
     deleteuser: function () {
-      AXIOS.delete(`/endUsers/`.concat(this.Username), {}, {})
+      AXIOS.delete(`/endUsers/`.concat(this.username), {}, {})
           .then((response) => {
             this.$router.push({name: "SignIn"});
           })
@@ -232,6 +232,19 @@ export default {
             this.errorMessage = error;
           });
            
+    },
+
+    formattedServices: function(appointment) {
+
+      var servicesString = "";
+
+      var i=0;
+      for (; i<appointment.services.length-1; i++) {
+          servicesString += appointment.services[i].name + ", ";
+      }
+      servicesString += appointment.services[i].name;
+
+      return servicesString;
     }
   }
 }
