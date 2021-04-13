9bb681b6bc39c24f9daef6cfb6cc67e02c9f4f5a
Ezra <ezra.gomolin@mail.mcgill.ca>
2021-04-13 10:16:24 -0400

    added login stuff and fixed gradle file

--- AutoRepairSystem-Android/app/src/main/java/ca/mcgill/ca/ecse321/autorepairsystem/HttpUtils.java
+++ AutoRepairSystem-Android/app/src/main/java/ca/mcgill/ca/ecse321/autorepairsystem/HttpUtils.java
@@ -1,4 +1,37 @@
 package ca.mcgill.ca.ecse321.autorepairsystem;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.AsyncHttpClient;
+
+
 public class HttpUtils {
+    public static final String DEFAULT_BASE_URL = "https://autorepairsystem-backend-fs173.herokuapp.com";
+
+    private static String baseUrl;
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    static {
+        baseUrl = DEFAULT_BASE_URL;
+    }
+
+    public static String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public static void setBaseUrl(String baseUrl) {
+        HttpUtils.baseUrl = baseUrl;
+    }
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return baseUrl + relativeUrl;
+    }
 }
--- AutoRepairSystem-Android/app/src/main/java/ca/mcgill/ca/ecse321/autorepairsystem/LogIn.java
+++ AutoRepairSystem-Android/app/src/main/java/ca/mcgill/ca/ecse321/autorepairsystem/LogIn.java
@@ -1,2 +1,77 @@
-public class LogIn {
-}
+package ca.mcgill.ca.ecse321.autorepairsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import androidx.appcompat.app.AppCompatActivity;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class LogIn extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private String user="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+
+    }
+
+    public void signIn(View v){
+        RequestParams parameters = new RequestParams();
+        parameters.add("username", username.getText().toString());
+        parameters.add("password", password.getText().toString());
+
+        HttpUtils.get("/signin", parameters, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //clear fields for next login
+                username.setText("");
+                password.setText("");
+                try {
+                    //store user email locally to be passed to other activities
+                    user = response.getString("username");
+                    //Redirect to customer page
+                    toCustomerPage();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                final TextView displayError = (TextView) findViewById(R.id.login);
+                //clear text boxes for next login attempt
+                username.setText("");
+                password.setText("");
+                displayError.append("Invalid Credentials");
+            }
+        });
+
+
+        }
+
+    public void toCustomerPage(){
+        System.exit(0);
+        //setContentView(R.layout.activity_login);
+        Intent intent= new Intent(this, LogIn.class);
+        startActivity(intent);
+    }
+
+
+    }
+
+
